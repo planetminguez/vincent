60ddddd1f6caf51d84f032d6018aa09288d3a958
Joshua John <planetminguez@gmail.com>
2023-06-02 04:12:03 -0400

    Add files via upload

---
+++ stuff.c
@@ -0,0 +1,12 @@
+/*
+* Copyright 2020, @planetminguez
+* gcc dtree_patcher.c -o dtree_patcher -g
+*/
+#include <stdio.h>
+
+int main(int argc, char **argv){
+    
+    
+        printf("\t\tChange System mount as a ro to rw \n");
+        return 0;
+    }
