c952edcb68d81dd38a24bd86a91890cc6038ee86
Joshua Minguez <planetminguez@gmail.com>
2024-10-07 05:48:36 -0400

    Update stuff.c
    Added strtohex func

--- stuff.c
+++ stuff.c
@@ -4,6 +4,34 @@
 */
 //Taking stuff out
 
+str2hex(int buflen, unsigned char *buf, const char *str)
+{
+    unsigned char *ptr = buf;
+    int seq = -1;
+    while (buflen > 0) {
+        int nibble = *str++;
+        if (nibble >= '0' && nibble <= '9') {
+            nibble -= '0';
+        } else {
+            nibble |= 0x20;
+            if (nibble < 'a' || nibble > 'f') {
+                break;
+            }
+            nibble -= 'a' - 10;
+        }
+        if (seq >= 0) {
+            *buf++ = (seq << 4) | nibble;
+            buflen--;
+            seq = -1;
+        } else {
+            seq = nibble;
+        }
+    }
+    return buf - ptr;
+}
+
+/////////////
+
 #if defined(__linux) || defined(__APPLE__) || defined(__CYGWIN__)
 #include <sys/time.h>
 #endif
